05e7dcb4972785551b7693aa6991b2208aafc38d
alex <support@mazurov.eu>
2016-02-13 08:44:12 +0200

    dev

--- usefull.bash
+++ usefull.bash
@@ -6,3 +6,7 @@ npm init -y
 cd 
 git add version_desk_react_00 && git commit -m "Initial dist subtree commit"
 git subtree push --prefix version_desk_react_00  origin version_desk_react_00
+
+cd version_desk_react_00
+
+npm install react react-bootstrap react-document-meta react-dom react-router --save
